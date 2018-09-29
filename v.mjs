737b5323300f48598a1e2e8f29cbc0da6d6e9848
Minifyre <minifyre@gmail.com>
2018-09-29 13:19:12 -0500

    curried functions

--- v.mjs
+++ v.mjs
@@ -1,4 +1,6 @@
-const {assign,entries,keys}=Object
+const
+{assign,entries,keys}=Object,
+curry=(fn,...xs)=>xs.length>=fn.length?fn(...xs):(...ys)=>curry(fn,...xs,...ys)
 export default function v(selector,props={},...children)
 {
 	const
@@ -6,7 +8,6 @@ export default function v(selector,props={},...children)
 	type=(nodeType||'div').toLowerCase(),
 	on=entries(props.on||{})
 	.reduce((obj,[type,fn])=>assign(obj,{[type]:Array.isArray(fn)?fn:[fn]}),{})
-	//@todo map objs to args arrays
 	delete props.on
 	entries(props.data||{}).forEach(([prop,val])=>props['data-'+prop]=val)
 	delete props.data
@@ -16,13 +17,14 @@ export default function v(selector,props={},...children)
 }
 v.util=
 {
+	curry,
 	equal:(a,b)=>JSON.stringify(a)===JSON.stringify(b),
-	setAll:(fn,el,obj)=>entries(obj).forEach(([prop,val])=>fn(el,prop,val))
-}
-v.util.update=function(fn,el,newObj,oldObj={})
-{
-	keys(assign({},newObj,oldObj))
-	.forEach(prop=>fn(el,prop,newObj[prop],oldObj[prop]))
+	setAll:(fn,el,obj)=>entries(obj).forEach(([prop,val])=>fn(el,prop,val)),
+	update:function(fn,el,newObj,oldObj={})
+	{
+		keys(assign({},newObj,oldObj))
+		.forEach(prop=>fn(el,prop,newObj[prop],oldObj[prop]))
+	}
 }
 v.changed=(a,b)=>typeof a!==typeof b||typeof a==='string'&&a!==b||a.type!==b.type
 v.el=function(node)
@@ -32,11 +34,7 @@ v.el=function(node)
 	const el=document.createElement(node.type)
 	v.setProps(el,node.props)//@todo chain these
 	v.setEvts(el,node.on)
-
-	node.children
-	.map(v.el)
-	.forEach(el.appendChild.bind(el))
-
+	node.children.forEach(child=>el.appendChild(v.el(child)))
 	return el
 }
 v.evtDel=(el,type,args)=>el.removeEventListener(type,...args)
@@ -47,18 +45,15 @@ v.propDel=function(el,prop,val)
 	else if(typeof val==='boolean') el[prop]=false
 	el.removeAttribute(prop)
 }
-v.propSet=function(el,prop,val)//@todo handle checked & value
+v.propSet=function(el,prop,val)
 {
 	if(prop==='value') el[prop]=val
 	else if(typeof val==='boolean') el[prop]=val
 	el.setAttribute(prop,val)
 }
-v.setEvts=(...args)=>v.util.setAll(v.evtSet,...args)
-v.setProps=(...args)=>v.util.setAll(v.propSet,...args)
 v.update=function(parent,newNode,oldNode,child=parent.childNodes[0])
 {
 	if(oldNode==null) parent.appendChild(v.el(newNode))
-	//@todo remove event listeners as well (for both remove & replace)
 	else if(newNode==null) return parent.removeChild(child),-1
 	else if(v.changed(newNode,oldNode)) parent.replaceChild(v.el(newNode),child)
 	else if(newNode.type)
@@ -78,9 +73,8 @@ v.update=function(parent,newNode,oldNode,child=parent.childNodes[0])
 			)
 		}
 	}
-	return 0// suggest that an element has not been removed
+	return 0//element has not been removed
 }
-v.updateEvts=(...args)=>v.util.update(v.updateEvt,...args)
 v.updateEvt=function(el,type,[newFn,...newOpts],[oldFn,...oldOpts])
 {
 	if(!newFn) v.evtDel(el,type,oldVal)
@@ -91,9 +85,12 @@ v.updateEvt=function(el,type,[newFn,...newOpts],[oldFn,...oldOpts])
 		v.evtSet(el,type,newVal)
 	}
 }
-v.updateProps=(...args)=>v.util.update(v.updateProp,...args)
 v.updateProp=function(el,prop,newVal,oldVal)
 {
 	if(!newVal) v.propDel(el,prop,oldVal)
 	else if(!oldVal||newVal!==oldVal) v.propSet(el,prop,newVal)
 }
+v.setEvts=curry(v.util.setAll,v.evtSet)
+v.setProps=curry(v.util.setAll,v.propSet)
+v.updateEvts=curry(v.util.update,v.updateEvt)
+v.updateProps=curry(v.util.update,v.updateProp)
